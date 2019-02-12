705bb905d35bc50b79c76bebe806d24e21f540a5
stevenJoyce <37178649+stevenJoyce@users.noreply.github.com>
2019-02-12 17:25:03 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+
+  counter: number = 0;
+
+  hide: boolean = true;
+
+  onButtonClick() {
+      this.counter++;
+  }// onSave()
+
+  clickImage() {
+console.log('Image Clicked');
+
+if (this.hide === false){
+  this.hide = true;
+} else {
+  this.hide = false;
 }
+}
+}// class
